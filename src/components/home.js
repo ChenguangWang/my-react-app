dead06852b9b05607d0bd51072d10a2fce6800fb
Wang Chenguang <chenguang.wang@tendcloud.com>
2023-02-20 10:43:04 +0800

    feat: commit

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Button } from "antd";
 
 export default class Home extends React.Component {
     render() {
         return (
             <div>
                 this home component
-                {this.props.children}
+                <div>
+                    <Button type="primary">Button</Button>
+                </div>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import RouterConfig from './router/router';
+/**引入 antd */
+import "antd/dist/antd.css";
 
 ReactDOM.render(
     <RouterConfig />,
